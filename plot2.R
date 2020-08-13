06da15fd5ff07d2438f2902f55044307c4628caa
Vasuda Dokiparthi <vasudok@gmail.com>
2020-08-12 20:17:46 -0700

    update plot 2

--- plot2.R
+++ plot2.R
@@ -4,6 +4,5 @@ workingset2$Time <- strptime(paste(workingset2$Date, workingset2$Time), format="
 png(filename = "/Users/vasudok/R Programming/Coursera R/EDAProject1/plot2.png")
 with(workingset2, plot(Time, Global_active_power, 
                          type="l",
-                         ylab="Global Active Power (in kilowatts)",
-                         cex.lab=0.7, cex.axis=0.8))
+                         ylab="Global Active Power (in kilowatts)")
 dev.off()
